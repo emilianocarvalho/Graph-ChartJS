3f7c39b69e6cbfda972109aaeff0e5897ef0b287
emilianocarvalho <emilianocarvalho.it@gmail.com>
2015-10-23 18:21:43 -0300

    Limpeza código

--- main.js
+++
@@ -1,9 +0,0 @@
-function nextPage(pagina) {
-    window.location.assign(pagina)
-}
-
-function criarLink(pagina)
-{
-	document.getElementById("link").innerHTML = "<a href="+pagina+">Proxima Pagina</a>";
-    document.getElementById("btnPg2").style.visibility='hidden'; // hide
-}
